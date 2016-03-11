5ae77b7a45cac7e56022c7492c4d015d3558941d
Laurence Charles <charles.laurence1@gmail.com>
2016-03-11 10:38:18 -0400

    ignore test

--- frontend/web/js/students/ajax_functions.js
+++ frontend/web/js/students/ajax_functions.js
@@ -132,7 +132,7 @@ function AddCsecQualificationAjaxFunction(e)
     //For live sat_dev implementation
 //    var url = "http://www.svgcc.vc/subdomains/sat_dev/frontend/web/index.php?r=subcomponents%2Fstudents%2Fprofile%2Fexamination-body-dependants&exam_body_id=";
     
-    //For live sat implementation
+    //Implementation for live server
     var url = "http://www.svgcc.vc/subdomains/sat/frontend/web/index.php?r=subcomponents%2Fstudents%2Fprofile%2Fexamination-body-dependants&exam_body_id=";
     
     
@@ -276,7 +276,7 @@ function EditCsecQualificationAjaxFunction(e)
     //For live sat_dev implementation
 //    var url = "http://www.svgcc.vc/subdomains/sat_dev/frontend/web/index.php?r=subcomponents%2Fstudents%2Fprofile%2Fexamination-body-dependants&exam_body_id=";
     
-    //Live sat implementation on server
+    //Implementation for live server
     var url = "http://www.svgcc.vc/subdomains/sat/frontend/web/index.php?r=subcomponents%2Fstudents%2Fprofile%2Fexamination-body-dependants&exam_body_id=";
      
        
