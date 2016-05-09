54b2a960068726f9ff8a665c8193887a0f328d0a
Laurence Charles <charles.laurence1@gmail.com>
2016-05-09 17:42:35 -0400

    Questionable OFfers Flag customized with offertype 7

--- frontend/models/CsecQualification.php
+++ frontend/models/CsecQualification.php
@@ -321,7 +321,7 @@ class CsecQualification extends \yii\db\ActiveRecord
                 ->innerJoin('academic_offering', '`academic_offering`.`academicofferingid` = `application`.`academicofferingid`')
                 ->innerJoin('application_period', '`application_period`.`applicationperiodid` = `academic_offering`.`applicationperiodid`')
                 ->where(['csec_qualification.candidatenumber' => $candidateno, 'csec_qualification.isdeleted' => 0,'csec_qualification.year' => $year,
-                        'application.isactive' => 1, 'application.isdeleted' => 0, 'application.applicationstatusid' => 11, 'application.personid' => $personid,
+                        'application.isactive' => 1, 'application.isdeleted' => 0, 'application.applicationstatusid' => [8,9], 'application.personid' => $personid,
                         'academic_offering.isdeleted' => 0
                         ])
                 ->groupBy('personid')
