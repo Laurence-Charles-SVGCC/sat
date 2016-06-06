9f7fcf5cda0d39f11fe0d954ab39fb31c69f0177
Laurence Charles <charles.laurence1@gmail.com>
2016-06-05 22:03:19 -0400

    Index page updated

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -10,7 +10,7 @@ $this->title = 'SVGCC Administrative Terminal';
 
     <div class="body-content">
         <h2>Please select a task from the left sidebar. If there are no tasks there, you may not have the proper permissions set. 
-            Please contact the E-College Unit </h2>  
+            Please contact the the E-College Unit for assistance. </h2>  
 
     </div>
 </div>
