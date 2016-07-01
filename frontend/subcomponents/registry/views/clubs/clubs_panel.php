a1d148115ce8104c6a26e487d6d5f0898e3a0312
Laurence Charles <charles.laurence1@gmail.com>
2016-07-01 14:57:37 -0400

    Image rendering on Club module v1

--- frontend/subcomponents/registry/views/clubs/clubs_panel.php
+++ frontend/subcomponents/registry/views/clubs/clubs_panel.php
@@ -14,9 +14,9 @@
         <div class = "custom_wrapper">
             <div class="custom_header">
                 <a href="<?= Url::toRoute(['/subcomponents/registry/clubs/manage-clubs']);?>" title="Manage Clubs">     
-                    <img class="custom_logo_students" src ="<?=Url::to('../images/club.png');?>" alt="club avatar">
+                    <img class="custom_logo_students" src ="css/dist/img/header_images/club.png" alt="club avatar">
                     <span class="custom_module_label" style="margin-left:8%;">Welcome to the Club Management System</span> 
-                    <img src ="<?=Url::to('../images/club.png');?>" alt="club avatar" class="pull-right">
+                    <img src ="css/dist/img/header_images/club.png" alt="club avatar" class="pull-right">
                 </a>    
             </div>
             
--- frontend/subcomponents/registry/views/clubs/configure_club.php
+++ frontend/subcomponents/registry/views/clubs/configure_club.php
@@ -17,9 +17,9 @@
         <div class = "custom_wrapper">
             <div class="custom_header">
                 <a href="<?= Url::toRoute(['/subcomponents/registry/awards/manage-awards']);?>" title="Manage Awards">     
-                    <img class="custom_logo_students" src ="<?=Url::to('../images/award.png');?>" alt="award avatar">
+                    <img class="custom_logo_students" src ="css/dist/img/header_images/club.png" alt="club avatar">
                     <span class="custom_module_label" style="margin-left:5%;"> Welcome to the Award Management System</span> 
-                    <img src ="<?=Url::to('../images/award.png');?>" alt="award avatar" class="pull-right">
+                    <img src ="css/dist/img/header_images/club.png" alt="club avatar" class="pull-right">
                 </a>    
             </div>
             
