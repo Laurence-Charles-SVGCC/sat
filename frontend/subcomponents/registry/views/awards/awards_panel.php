7219d81ff536b8a0fdb0d7bc3158c363b9829cbd
Laurence Charles <charles.laurence1@gmail.com>
2016-07-01 14:28:15 -0400

    Image rendering patch v4

--- frontend/subcomponents/registry/views/awards/awards_panel.php
+++ frontend/subcomponents/registry/views/awards/awards_panel.php
@@ -22,10 +22,11 @@
         <div class = "custom_wrapper">
             <div class="custom_header">
                 <a href="<?= Url::toRoute(['/subcomponents/registry/awards/manage-awards']);?>" title="Manage Awards">     
-                    <img class="custom_logo_students" src ="<?=Url::to('../images/award.png');?>" alt="award avatar">
+                    <!--<img class="custom_logo_students" src ="<?=Url::to('../images/award.png');?>" alt="award avatar">-->
+                    <img class="custom_logo_students" src ="css/dist/img/award.png" alt="award avatar">
                     <span class="custom_module_label" style="margin-left:5%;"> Welcome to the Award Management System</span> 
 <!--                    <img src ="<?=Url::to('../images/award.png');?>" alt="award avatar" class="pull-right">-->
-                    <img src ="css/dist/img/award.png" alt="award avatar" class="pull-right">
+                     <img src ="css/dist/img/award.png" alt="award avatar" class="pull-right">
                 </a>    
             </div>
             
