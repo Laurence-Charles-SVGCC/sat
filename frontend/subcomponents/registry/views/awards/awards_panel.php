f8176efb1829f37e51d1ac45cb3194ee1300840e
Laurence Charles <charles.laurence1@gmail.com>
2016-07-01 14:09:15 -0400

    Image rendering patch v1

--- frontend/subcomponents/registry/views/awards/awards_panel.php
+++ frontend/subcomponents/registry/views/awards/awards_panel.php
@@ -24,7 +24,8 @@
                 <a href="<?= Url::toRoute(['/subcomponents/registry/awards/manage-awards']);?>" title="Manage Awards">     
                     <img class="custom_logo_students" src ="<?=Url::to('../images/award.png');?>" alt="award avatar">
                     <span class="custom_module_label" style="margin-left:5%;"> Welcome to the Award Management System</span> 
-                    <img src ="<?=Url::to('../images/award.png');?>" alt="award avatar" class="pull-right">
+<!--                    <img src ="<?=Url::to('../images/award.png');?>" alt="award avatar" class="pull-right">-->
+                    <img src ="<?=Yii::getAlias('@frontend') . '/images/award.png';?>" alt="award avatar" class="pull-right">
                 </a>    
             </div>
             
