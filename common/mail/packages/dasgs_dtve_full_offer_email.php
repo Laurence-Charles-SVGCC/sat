b8653555eb078f613b84b95780c60e1aef465336
Laurence Charles <charles.laurence1@gmail.com>
2016-07-01 10:23:05 -0400

    Package manager update v5

--- common/mail/packages/dasgs_dtve_full_offer_email.php
+++ common/mail/packages/dasgs_dtve_full_offer_email.php
@@ -13,6 +13,12 @@
             <div id="introduction">
                 <p><?= date("l F j, Y"); ?></p>
                 <p>Dear <?= $first_name . ' ' . $last_name ?>,</p>
+
+                <p>
+                    We are pleased to inform you that your application to the St. Vincent and the Grenadines Community College has been successful.  
+                    You are offered a place in the <?= $programme; ?> at the <?= $division_name ?> commencing on <?= $package->commencementdate?>.<br/>
+                    Your Student Number is: <?= $studentno; ?>.
+                </p>
             </div>
 
             <div id="body" style="white-space: pre-wrap;">
