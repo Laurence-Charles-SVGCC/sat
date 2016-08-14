cdf42973f2723e4d52fd0824fcb982d42bdb0624
Laurence Charles <charles.laurence1@gmail.com>
2016-08-14 16:13:01 -0400

    Patching the ajax production environment failure v16

--- frontend/web/js/admissions/period_setup_one.js
+++ frontend/web/js/admissions/period_setup_one.js
@@ -164,8 +164,8 @@ function calculateApplicantIntent(e)
         url = "http://localhost:80/sat_dev/frontend/web/index.php?r=subcomponents%2Fadmissions%2Fadmissions%2Fprocess-applicant-intentid&";
     else
 //        url = "http://www.svgcc.vc/subdomains/sat/frontend/web/index.php?r=subcomponents%2Fadmissions%2Fadmissions%2Fprocess-applicant-intentid&";
-        url = "http://www.svgcc.vc/subdomains/sat/frontend/web/index.php?r=subcomponents%2Fadmissions%2Fajax%2Fprocess-applicant-intentid&";
-
+//        url = "http://www.svgcc.vc/subdomains/sat/frontend/web/index.php?r=subcomponents%2Fadmissions%2Fajax%2Fprocess-applicant-intentid&";
+        url = "http://sat.svgcc.vc/index.php?r=subcomponents%2Fadmissions%2Fadmissions%2Fprocess-applicant-intentid&";
 
 
 
