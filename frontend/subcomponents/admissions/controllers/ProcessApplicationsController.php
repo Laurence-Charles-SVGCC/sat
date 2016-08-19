cbd85f37477839fee01b6222456186bd49c4a349
Laurence Charles <charles.laurence1@gmail.com>
2016-08-19 08:56:57 -0400

    ProcessApplications patch

--- frontend/subcomponents/admissions/controllers/ProcessApplicationsController.php
+++ frontend/subcomponents/admissions/controllers/ProcessApplicationsController.php
@@ -276,11 +276,11 @@
                         ->innerJoin('academic_offering', '`application`.`academicofferingid` = `academic_offering`.`academicofferingid`')
                         ->innerJoin('application_period', '`academic_offering`.`applicationperiodid` = `application_period`.`applicationperiodid`')
                         ->where(['application_period.iscomplete' => 0, 'application_period.isactive' => 1, 'application_period.isdeleted' => 0,
-                                /*'application.isactive' => 1,*/ 'application.isdeleted' => 0, 'application.personid' => $applicant->personid])
+                                /*'application.isactive' => 1,*/ 'application.isdeleted' => 0, 'application.personid' => $personid])
                         ->orderBy('application.ordering ASC')
                         ->all();
             
-            $certificates = CsecQualification::getSubjects($applicant->personid);
+            $certificates = CsecQualification::getSubjects($personid);
             
             $application_container = array();
             
