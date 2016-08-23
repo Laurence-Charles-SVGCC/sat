5229a2cd922fa7ed855954a5f62b62b58ba6dfb9
Laurence Charles <charles.laurence1@gmail.com>
2016-08-23 09:06:48 -0400

    PAtched verifier field for applicant records v1

--- frontend/subcomponents/admissions/controllers/VerifyApplicantsController.php
+++ frontend/subcomponents/admissions/controllers/VerifyApplicantsController.php
@@ -409,7 +409,7 @@ class VerifyApplicantsController extends \yii\web\Controller
                      }
                      else
                      {
-                         $verifier = Employee::getEmployeeName(Yii::$app->user->identity->personid);
+                         $verifier = Employee::getEmployeeName($applicant->verifier);
                      }
                  }
                  $container["verifier"] = $verifier;
@@ -473,7 +473,7 @@ class VerifyApplicantsController extends \yii\web\Controller
                         }
                         else
                         {
-                            $verifier = Employee::getEmployeeName(Yii::$app->user->identity->personid);
+                            $verifier = Employee::getEmployeeName($applicant->verifier);
                         }
                     }
                     $container["verifier"] = $verifier;
@@ -714,7 +714,7 @@ class VerifyApplicantsController extends \yii\web\Controller
                 }
                 else
                 {
-                    $verifier = Employee::getEmployeeName(Yii::$app->user->identity->personid);
+                    $verifier = Employee::getEmployeeName($applicant->verifier);
                 }
                 $container["verifier"] = $verifier;
 
