7efad80352fd7e03f64988271bcf443257ac4675
Laurence Charles <charles.laurence1@gmail.com>
2016-08-30 14:03:09 -0400

    Restrict the editting of applicants programme choices after an offer is sent v7

--- frontend/subcomponents/admissions/views/process-applications/view_applicant_certificates.php
+++ frontend/subcomponents/admissions/views/process-applications/view_applicant_certificates.php
@@ -197,9 +197,10 @@
                    </p>
                 <?php endif;?>
             <?php endif;?>
+                
                 <h2 class="custom_h2">
                     Applications
-                    <?php if(Applicant::isVerified($applicant->personid) == true  && Applicant::getApplicantIntent($applicant->personid)  == 1  && ($division_id == 1 || ($division_id != 1 && $target_application->divisionid == $division_id))):?>
+                    <?php if(Applicant::hasBeenIssuedOffer($applicant->personid) == false  && Applicant::isVerified($applicant->personid) == true  && Applicant::getApplicantIntent($applicant->personid)  == 1  && ($division_id == 1 || ($division_id != 1 && $target_application->divisionid == $division_id))):?>
                         <div class="pull-right" style="margin-right:2.5%">
                             <?=Html::a(' Create Custom Offer', 
                                         ['process-applications/custom-offer', 'personid' => $applicant->personid, 'programme' => $programme, 'application_status' => $application_status], 
