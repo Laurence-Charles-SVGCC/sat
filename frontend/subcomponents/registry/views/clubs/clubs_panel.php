fd6d1c01935a62015b1d8348081dbefaaef2aa35
Laurence Charles <charles.laurence1@gmail.com>
2017-01-25 14:56:33 -0400

    Upgraded UI for ClubManagement

--- frontend/subcomponents/registry/views/clubs/clubs_panel.php
+++ frontend/subcomponents/registry/views/clubs/clubs_panel.php
@@ -1,5 +1,5 @@
-<?php
-
+ <?php
+    use yii\widgets\Breadcrumbs;
     use yii\helpers\Html;
     use yii\helpers\Url;
     use yii\widgets\ActiveForm;
@@ -7,79 +7,80 @@
     use frontend\models\Club;
     
     $this->title = 'Clubs Control Panel';
+    $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 
-    <div class="site-index">
-        <div class = "custom_wrapper">
-            <div class="custom_header">
-                <a href="<?= Url::toRoute(['/subcomponents/registry/clubs/manage-clubs']);?>" title="Manage Clubs">     
-                    <img class="custom_logo_students" src ="css/dist/img/header_images/club.png" alt="club avatar">
-                    <span class="custom_module_label" style="margin-left:8%;">Welcome to the Club Management System</span> 
-                    <img src ="css/dist/img/header_images/club.png" alt="club avatar" class="pull-right">
-                </a>    
-            </div>
-            
-            <div class="custom_body">  
-                <h1 class="custom_h1"><?=$this->title?></h1>
-                
-                </br>                              
-                <div class="panel panel-default" style="width:95%; margin: 0 auto;">
-                    <div class="panel-heading" style="color:green;font-weight:bold; font-size:1.3em">Clubs Listing
-                        <?php if(Yii::$app->user->can('createClub')):?>
-                            <a class="btn btn-success glyphicon glyphicon-plus pull-right" href=<?=Url::toRoute(['/subcomponents/registry/clubs/configure-club', 'action' => 'create']);?> role="button"> Create Club</a>
-                        <?php endif;?>
-                    </div>
-                    
-                    </br>
-                    <?php if($clubs == false):?>
-                        <h3>No clubs have been created</h3>
-                        
-                    <?php else:?>
-                        <table class='table table-condensed' style='margin: 0 auto;'>
-                            <?php foreach($clubs as $club):?>
-                                <tr>
-                                    <th rowspan='3' style='vertical-align:top; text-align:center; font-size:1.2em; width:20%'><?=$club->name?></th>
-                                    <th>Founded</th>
-                                    <td><?=$club->yearfounded;?></td>
-                                    <th>Description</th>
-                                    <td><?=$club->description?></td>
-                                </tr>
-                                
-                                <tr>
-                                    <th>Motto</th>
-                                    <td colspan='3' ><?=$club->motto;?></td>
-                                </tr>
-                                
-                                <tr>
-                                    <th>Division</th>
-                                    <td><?=Club::getDivision($club->clubid);?></td>
-                                    
-                                    <th>Action</th>
-                                    <td>
-                                        <div class='dropdown'>
-                                            <button class='btn btn-default dropdown-toggle' type='button' id='dropdownMenu1' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>
-                                                Select your intended action
-                                                <span class='caret'></span>
-                                            </button>
-                                            <ul class='dropdown-menu' aria-labelledby='dropdownMenu1'>
-                                                <?php if(Yii::$app->user->can('editClub')):?>
-                                                    <li><a href=<?=Url::toRoute(['/subcomponents/registry/clubs/configure-club', 'action' => 'edit', 'recordid' => $club->clubid])?>>Edit</a></li>
-                                                <?php endif;?>
-                                                    
-                                                <?php if(Club::hasMembers($club->clubid) == false  && Yii::$app->user->can('deleteClub')):?>    
-                                                    <li><a href=<?=Url::toRoute(['/subcomponents/registry/clubs/delete-club', 'recordid' => $club->clubid])?>>Delete Club</a></li>
-                                                <?php endif;?>
-                                            </ul>
-                                        </div>
-                                    </td>
-                                </tr>
-                            <?php endforeach;?>
-                        </table>
-                    <?php endif;?>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="page-header text-center no-padding">
+    <a href="<?= Url::toRoute(['/subcomponents/registry/clubs/manage-clubs']);?>" title="Manage Clubs">
+        <h1>Club Management System</h1>
+    </a>
+</div>
 
+<section class="content-header">
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
+</section><br/><br/>
+
+<div class="box box-primary table-responsive no-padding" style="font-size:1.1em">
+    <div class="box-header with-border">
+        <span class="box-title"><?= $this->title?></span>
+     </div>
+    
+    <div class="box-body">
+        </br>                              
+        <div class="panel panel-default">
+            <div class="panel-heading" style="color:green;font-weight:bold; font-size:1.3em">Clubs Listing
+                <?php if(Yii::$app->user->can('createClub')):?>
+                    <a class="btn btn-success glyphicon glyphicon-plus pull-right" href=<?=Url::toRoute(['/subcomponents/registry/clubs/configure-club', 'action' => 'create']);?> role="button"> Create Club</a>
+                <?php endif;?>
+            </div>
+
+            </br>
+            <?php if($clubs == false):?>
+                <p><strong>No clubs have been created</strong></p>
+
+            <?php else:?>
+                <table class='table table-condensed' style='margin: 0 auto;'>
+                    <?php foreach($clubs as $club):?>
+                        <tr>
+                            <th rowspan='3' style='vertical-align:top; text-align:center; font-size:1.2em; width:20%'><?=$club->name?></th>
+                            <th>Founded</th>
+                            <td><?=$club->yearfounded;?></td>
+                            <th>Description</th>
+                            <td><?=$club->description?></td>
+                        </tr>
+
+                        <tr>
+                            <th>Motto</th>
+                            <td colspan='3' ><?=$club->motto;?></td>
+                        </tr>
+
+                        <tr>
+                            <th>Division</th>
+                            <td><?=Club::getDivision($club->clubid);?></td>
+
+                            <th>Action</th>
+                            <td>
+                                <div class='dropdown'>
+                                    <button class='btn btn-default dropdown-toggle' type='button' id='dropdownMenu1' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>
+                                        Select your intended action
+                                        <span class='caret'></span>
+                                    </button>
+                                    <ul class='dropdown-menu' aria-labelledby='dropdownMenu1'>
+                                        <?php if(Yii::$app->user->can('editClub')):?>
+                                            <li><a href=<?=Url::toRoute(['/subcomponents/registry/clubs/configure-club', 'action' => 'edit', 'recordid' => $club->clubid])?>>Edit</a></li>
+                                        <?php endif;?>
+
+                                        <?php if(Club::hasMembers($club->clubid) == false  && Yii::$app->user->can('deleteClub')):?>    
+                                            <li><a href=<?=Url::toRoute(['/subcomponents/registry/clubs/delete-club', 'recordid' => $club->clubid])?>>Delete Club</a></li>
+                                        <?php endif;?>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endforeach;?>
+                </table>
+            <?php endif;?>
+        </div>
+    </div><br/>
+ </div>
--- frontend/subcomponents/registry/views/clubs/configure_club.php
+++ frontend/subcomponents/registry/views/clubs/configure_club.php
@@ -1,5 +1,5 @@
 <?php
-
+    use yii\widgets\Breadcrumbs;
     use yii\helpers\Html;
     use yii\helpers\Url;
     use yii\widgets\ActiveForm;
@@ -9,64 +9,59 @@
     use frontend\models\Division;
     
     $this->title = $action . " Club";
-
+    $this->params['breadcrumbs'][] = ['label' => 'Find A Student', 'url' => Url::toRoute(['/subcomponents/registry/clubs/manage-clubs'])];
+    $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 
-    <div class="site-index">
-        <div class = "custom_wrapper">
-            <div class="custom_header">
-                <a href="<?= Url::toRoute(['/subcomponents/registry/awards/manage-awards']);?>" title="Manage Awards">     
-                    <img class="custom_logo_students" src ="css/dist/img/header_images/club.png" alt="club avatar">
-                    <span class="custom_module_label" style="margin-left:5%;"> Welcome to the Award Management System</span> 
-                    <img src ="css/dist/img/header_images/club.png" alt="club avatar" class="pull-right">
-                </a>    
-            </div>
-            
-            <div class="custom_body">  
-                <h1 class="custom_h1"><?=$this->title?></h1>
-                
-                </br>                              
-                <?php
-                    $form = ActiveForm::begin([
-                        'id' => 'configure-club-form',
-                        'options' => [
-                            'style' => 'width:80%; margin:0 auto;',
-                        ],
-                    ]);
-                ?>
-                
-                    <table class='table table-hover' style='width:100%; margin: 0 auto;'>
-                        <tr>
-                            <th style='width:30%; vertical-align:middle'>Name</th>
-                            <td><?=$form->field($club, 'name')->label('', ['class'=> 'form-label'])->textInput(['maxlength' => true, 'style' => 'vertical-align:middle'])?></td>
-                        </tr>
-                        
-                        <tr>
-                            <th style='width:30%; vertical-align:middle'>Description</th>
-                            <td><?=$form->field($club, 'description')->label('', ['class'=> 'form-label'])->textArea(['maxlength' => true, 'style' => 'vertical-align:middle', 'rows' => 10])?></td>
-                        </tr>
-                        
-                        <tr>
-                            <th style='width:30%; vertical-align:middle'>Motto</th>
-                            <td><?=$form->field($club, 'motto')->label('', ['class'=> 'form-label'])->textArea(['maxlength' => true, 'style' => 'vertical-align:middle', 'rows' => 10])?></td>
-                        </tr>
-                        
-                        <tr>
-                            <th style='width:30%; vertical-align:middle'>Year Founded</th>
-                            <td><?=$form->field($club, 'yearfounded')->label('', ['class'=> 'form-label'])->textInput(['maxlength' => true, 'style' => 'vertical-align:middle'])?></td>
-                        </tr>
-                        
-                        <tr>
-                            <th style='width:30%; vertical-align:middle'>Division</th>
-                            <td><?=$form->field($clubdivision, 'divisionid')->label('')->dropDownList(ArrayHelper::map(Division::find()->where(['divisionid' => [1,4,5,6,7]])->all(), 'divisionid', 'name'), ['prompt'=>'Select Division', 'onchange' => 'toggleAwardType();']) ?></td>
-                        </tr> 
-                    </table><br/>
-                    
-                    <?= Html::a(' Cancel',['clubs/manage-clubs'], ['class' => 'btn btn-block btn-lg btn-danger glyphicon glyphicon-remove-circle pull-right', 'style' => 'width:20%; margin-left:5%;']);?>
-                    <?= Html::submitButton(' Save', ['class' => 'btn btn-block btn-lg btn-success pull-right', 'style' => 'width:20%;']);?>
-                <?php ActiveForm::end(); ?>
-            </div>
-        </div>
-    </div>
+<div class="page-header text-center no-padding">
+    <a href="<?= Url::toRoute(['/subcomponents/registry/clubs/manage-clubs']);?>" title="Manage Clubs">
+        <h1>Club Management System</h1>
+    </a>
+</div>
 
+<section class="content-header">
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
+</section><br/><br/>
+
+<div class="box box-primary table-responsive no-padding" style="font-size:1.1em">
+    <div class="box-header with-border">
+        <span class="box-title"><?= $this->title?></span>
+     </div>
+    
+    <?php $form = ActiveForm::begin();?>
+        <div class="box-body">
+            <div class="form-group">
+               <label class="control-label col-xs-6 col-sm-5 col-md-5 col-lg-3" for="name">Name:</label>
+               <?=$form->field($club, 'name')->label('', ['class'=> 'form-label'])->textInput(["class" => "no-padding col-xs-6 col-sm-7 col-md-7 col-lg-9"])?>
+           </div>
+            
+            <div class="form-group">
+               <label class="control-label col-xs-6 col-sm-5 col-md-5 col-lg-3" for="description">Description:</label>
+               <?=$form->field($club, 'description')->label('', ['class'=> 'form-label'])->textArea(["rows" => 5, "class" => "no-padding col-xs-6 col-sm-7 col-md-7 col-lg-9"])?>
+           </div>
+            
+            <div class="form-group">
+               <label class="control-label col-xs-6 col-sm-5 col-md-5 col-lg-3" for="motto">Motto:</label>
+               <?=$form->field($club, 'motto')->label('', ['class'=> 'form-label'])->textArea(['rows' => 5, "class" => "no-padding col-xs-6 col-sm-7 col-md-7 col-lg-9" ])?>
+           </div>
+            
+            <div class="form-group">
+               <label class="control-label col-xs-6 col-sm-5 col-md-5 col-lg-3" for="yearfounded">Year Founded:</label>
+               <?=$form->field($club, 'yearfounded')->label('', ['class'=> 'form-label'])->textInput(["class" => "no-padding col-xs-6 col-sm-7 col-md-7 col-lg-9"])?>
+           </div>
+            
+            <div class="form-group">
+               <label class="control-label col-xs-6 col-sm-5 col-md-5 col-lg-3" for="'divisionid">Division:</label>
+               <?=$form->field($clubdivision, 'divisionid')->label('')->dropDownList(ArrayHelper::map(Division::find()->where(['divisionid' => [1,4,5,6,7]])->all(), 'divisionid', 'name'), ['prompt'=>'Select Division', "class" => "no-padding col-xs-6 col-sm-7 col-md-7 col-lg-9"]) ?>
+           </div>
+       </div>
+        
+         <div class="box-footer">
+            <span class = "pull-right">
+                <?= Html::submitButton(' Submit', ['class' => 'btn btn-success', 'style' => 'margin-right:20px']);?>
+                <?= Html::a(' Cancel', ['clubs/manage-clubs'], ['class' => 'btn  btn-danger']);?>
+            </span>
+        </div>
+    <?php ActiveForm::end(); ?>
+</div>
