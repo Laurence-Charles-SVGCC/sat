1dde3466e3abbd3956e618e3164ff5626b0c561e
Laurence Charles <charles.laurence1@gmail.com>
2017-01-26 21:27:29 -0400

    Upgraded UI for Admissions-SnapshotReports

--- frontend/subcomponents/admissions/views/reports/applicants_snapshot_results.php
+++ frontend/subcomponents/admissions/views/reports/applicants_snapshot_results.php
@@ -1,5 +1,5 @@
 <?php
-
+    use yii\widgets\Breadcrumbs;
     use yii\helpers\Html;
     use yii\helpers\Url;
     use kartik\grid\GridView;
@@ -9,104 +9,99 @@
     use frontend\models\Application;
     
     $this->title = $header;
+    $this->params['breadcrumbs'][] = ['label' => 'Report Dashboard', 'url' => Url::toRoute(['/subcomponents/admissions/reports/snapshot'])];
+    $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="body-content">
-    <div class = "custom_wrapper">
-        <div class="custom_header">
-            <a href="<?= Url::toRoute(['/subcomponents/admissions/admissions/index']);?>" title="Admissions Home">     
-                <img class="custom_logo_students" src ="css/dist/img/header_images/admissions.png" alt="admission-avatar">
-                <span class="custom_module_label">Welcome to the Admissions Management System</span> 
-                <img src ="css/dist/img/header_images/admissions.png" alt="admission-avatar" class="pull-right">
-            </a>    
-        </div>
-        
-        <div class="custom_body">
-            <h1 class="custom_h1"><?= Html::encode($this->title) ?></h1>
-            
-            <div id="listing">
-                <h2 class="custom_h2" style="margin-left:2.5%">Applicant Listing</h2>
-                
-                    <?= GridView::widget([
-                            'dataProvider' => $dataProvider,
-                            'options' => ['style' => 'width: 100%; margin: 0 auto;'],
-                            'columns' => [
-                                [
-                                    'attribute' => 'username',
-                                    'format' => 'text',
-                                    'label' => 'Username'
-                                ],
-                                [
-                                    'attribute' => 'title',
-                                    'format' => 'text',
-                                    'label' => 'Title'
-                                ],
-                                [
-                                    'attribute' => 'firstname',
-                                    'format' => 'text',
-                                    'label' => 'First Name'
-                                ],
-                                [
-                                    'attribute' => 'lastname',
-                                    'format' => 'text',
-                                    'label' => 'Last Name'
-                                ],
-                                [
-                                    'attribute' => 'programme',
-                                    'format' => 'text',
-                                    'label' => 'Programme'
-                                ],
-                                [
-                                    'attribute' => 'email',
-                                    'format' => 'text',
-                                    'label' => 'Email'
-                                ],
-                                [
-                                    'attribute' => 'phone',
-                                    'format' => 'text',
-                                    'label' => 'Phone Number(s)'
-                                ],
-                            ],
-                        ]); 
-                    ?>
-                
-                
-                    <?php if ($dataProvider):?>
-                        <h2 class="custom_h2" style="margin-left:2.5%">Export List</h2>
-                        <?php $form = ActiveForm::begin([
-                            'action' => Url::to(['reports/export-snapshot', 'selected_ordering' => $selected_ordering]),
-                        ]);?>
-                            <div style="width:95%; margin: 0 auto"><br/>
-                                <fieldset>
-                                    <legend>1. Select one or more programmes for search:</legend>
-                                    <div class="row">
-                                        <div class="col-lg-9">
-                                            <?= Html::checkboxList('offerings', null, $listing, []);?>
-                                        </div>
-                                    </div>
-                                </fieldset><br/>
-                                
-<!--                                <fieldset>
-                                    <legend>2. Select priority of programme search:</legend>
-                                    <div class="row">
-                                        <div class="col-lg-3">
-                                            <?= Html::radioList('ordering', null, [1 => 'First Choice', 2 => 'Second Choice', 3 => 'Child Choice'], ['class'=> 'form_field']);?>
-                                        </div>
-                                    </div>
-                                </fieldset>-->
 
-                                 <div class="form-group">
-                                    <br/><?= Html::submitButton('Search', ['class' => 'btn btn-md btn-success', 'style' => 'float: left']) ?>
-                                </div>
-                            </div>
-                        <?php ActiveForm::end(); ?>
-                            
-                        
-                        
-                    <?php endif;?>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="page-header text-center no-padding">
+    <a href="<?= Url::toRoute(['/subcomponents/admissions/reports/snapshot']);?>" title="Snapshot Reports Home">
+        <h1>Welcome to the Admissions Management System</h1>
+    </a>
 </div>
 
+<section class="content-header">
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
+</section><br/>
+
+<h2 class="text-center"><?=$this->title?></h2>
+
+<div class="box box-primary table-responsive no-padding" style = "font-size:1.2em;">
+    <div class="box-header with-border">
+        <span class="box-title">Applicant Listing</span>
+    </div>
+    
+    <div class="box-body" id="listing" style="width:98%; margin: 0 auto;">
+        <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'options' => ['style' => 'width: 100%; margin: 0 auto;'],
+                'columns' => [
+                    [
+                        'attribute' => 'username',
+                        'format' => 'text',
+                        'label' => 'Username'
+                    ],
+                    [
+                        'attribute' => 'title',
+                        'format' => 'text',
+                        'label' => 'Title'
+                    ],
+                    [
+                        'attribute' => 'firstname',
+                        'format' => 'text',
+                        'label' => 'First Name'
+                    ],
+                    [
+                        'attribute' => 'lastname',
+                        'format' => 'text',
+                        'label' => 'Last Name'
+                    ],
+                    [
+                        'attribute' => 'programme',
+                        'format' => 'text',
+                        'label' => 'Programme'
+                    ],
+                    [
+                        'attribute' => 'email',
+                        'format' => 'text',
+                        'label' => 'Email'
+                    ],
+                    [
+                        'attribute' => 'phone',
+                        'format' => 'text',
+                        'label' => 'Phone Number(s)'
+                    ],
+                ],
+            ]); 
+        ?>
+
+        <?php if ($dataProvider):?>
+            <h3 style="margin-left:2.5%">Export List</h3>
+            <?php $form = ActiveForm::begin(['action' => Url::to(['reports/export-snapshot', 'selected_ordering' => $selected_ordering]),]);?>
+                <div style="width:98%; margin: 0 auto"><br/>
+                    <fieldset>
+                        <legend>1. Select one or more programmes for search:</legend>
+                        <div class="row">
+                            <div class="col-lg-9">
+                                <?= Html::checkboxList('offerings', null, $listing, []);?>
+                            </div>
+                        </div>
+                    </fieldset>
+
+<!--                                <fieldset>
+                        <legend>2. Select priority of programme search:</legend>
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <?= Html::radioList('ordering', null, [1 => 'First Choice', 2 => 'Second Choice', 3 => 'Child Choice'], ['class'=> 'form_field']);?>
+                            </div>
+                        </div>
+                    </fieldset>-->
+
+                     <div class="form-group">
+                        <?= Html::submitButton('Search', ['class' => 'btn btn-md btn-success', 'style' => 'float: left']) ?>
+                    </div>
+                </div>
+            <?php ActiveForm::end(); ?>
+        <?php endif;?>
+   </div>
+</div>
--- frontend/subcomponents/admissions/views/reports/find_applicants_snapshot.php
+++ frontend/subcomponents/admissions/views/reports/find_applicants_snapshot.php
@@ -1,5 +1,5 @@
 <?php
-
+    use yii\widgets\Breadcrumbs;
     use yii\helpers\Html;
     use yii\widgets\ActiveForm;
     use yii\helpers\Url;
@@ -8,55 +8,60 @@
     use frontend\models\ApplicationPeriod;
 
     $this->title = 'Applicant Snapshot Generator';
-    //$this->params['breadcrumbs'][] = $this->title;
+    $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="report-index">
-    <div class = "custom_wrapper">
-        <div class="custom_header">
-            <a href="<?= Url::toRoute(['/subcomponents/admissions/admissions/index']);?>" title="Admissions Home">     
-                <img class="custom_logo_students" src ="css/dist/img/header_images/admissions.png" alt="admission-avatar">
-                <span class="custom_module_label">Welcome to the Admissions Management System</span> 
-                <img src ="css/dist/img/header_images/admissions.png" alt="admission-avatar" class="pull-right">
-            </a>    
-        </div>
-        
-        <div class="custom_body">
-            <h1 class="custom_h1"><?= Html::encode($this->title) ?></h1>
-           
-            <?php $form = ActiveForm::begin();?>
-            
-                <div class="alert in alert-block fade alert-info mainButtons" style="width:95%; margin: 0 auto">
-                    This report generator is intended for uses to generate a snapshot report of Applicant programme choices based on
-                    the name of the programme and priority of the choice.  Please select the programmes you wish to investigate from the
-                    checklist and their priority.
-                </div>
-            
-                <div style="width:95%; margin: 0 auto"><br/>
-                    <fieldset>
-                        <legend>1. Select one or more programmes for search:</legend>
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <?= Html::checkboxList('offerings', null, $listing, []);?>
-                            </div>
-                        </div>
-                    </fieldset><br/>
 
-                    <fieldset>
-                        <legend>2. Select priority of programme search:</legend>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <?= Html::radioList('ordering', null, [1 => 'First Choice', 2 => 'Second Choice', 3 => 'Child Choice'], ['class'=> 'form_field']);?>
-                            </div>
-                        </div>
-                    </fieldset>
-                        
-                     <div class="form-group">
-                        <br/><?= Html::submitButton('Search', ['class' => 'btn btn-md btn-success', 'style' => 'float: left']) ?>
-                    </div>
-                </div>
-            <?php ActiveForm::end(); ?>
-            
-        </div>
+<div class="page-header text-center no-padding">
+    <a href="<?= Url::toRoute(['/subcomponents/admissions/reports/snapshot']);?>" title="Snapshot Reports Home">
+        <h1>Welcome to the Admissions Management System</h1>
+    </a>
+</div>
+
+<section class="content-header">
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
+</section><br/>
+
+<h2 class="text-center"><?=$this->title?></h2>
+
+<div class="box box-primary table-responsive no-padding" style = "font-size:1.2em;">
+    <div class="box-header with-border">
+        <span class="box-title"><?= $this->title?></span>
     </div>
+    
+    <?php $form = ActiveForm::begin();?>
+        <div class="box-body">
+            <div class="alert in alert-block fade alert-info mainButtons" style="width:95%; margin: 0 auto">
+                This report generator is intended for uses to generate a snapshot report of Applicant programme choices based on
+                the name of the programme and priority of the choice.  Please select the programmes you wish to investigate from the
+                checklist and their priority.
+            </div>
+            
+            <div style="width:95%; margin: 0 auto"><br/>
+                <fieldset>
+                    <legend>1. Select one or more programmes for search:</legend>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <?= Html::checkboxList('offerings', null, $listing, []);?>
+                        </div>
+                    </div>
+                </fieldset><br/>
+
+                <fieldset>
+                    <legend>2. Select priority of programme search:</legend>
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <?= Html::radioList('ordering', null, [1 => 'First Choice', 2 => 'Second Choice', 3 => 'Child Choice'], ['class'=> 'form_field']);?>
+                        </div>
+                    </div>
+                </fieldset>
+            </div>
+        </div> 
+                
+        <div class="box-footer">
+            <span class = "pull-right">
+                <?= Html::submitButton('Search', ['class' => 'btn btn-success', 'style' => 'margin-right:20px']);?>
+            </span>
+        </div>
+    <?php ActiveForm::end(); ?>
 </div>
