10377f374180b5c43cab3a903c8e86d35a5f685f
Laurence Charles <charles.laurence1@gmail.com>
2017-01-30 10:45:41 -0400

    Patched StudentProfile

--- frontend/subcomponents/admissions/views/verify-applicants/centre-details.php
+++ frontend/subcomponents/admissions/views/verify-applicants/centre-details.php
@@ -1,61 +1,58 @@
 <?php
-
+    use yii\widgets\Breadcrumbs;
     use yii\helpers\Html;
     use yii\helpers\Url;
-
-    /* @var $this yii\web\View */
-    /* @var $searchModel frontend\models\CsecCentreSearch */
-    /* @var $dataProvider yii\data\ActiveDataProvider */
 
     if (strcasecmp($centre_name, "External") == 0)
         $this->title = 'Details of: Applicants With External Qualifications';
     else
         $this->title = 'Details of: ' . $centre_name;
+    
+    $this->params['breadcrumbs'][] = ['label' => 'Examination Centre Listing', 'url' => ['index']];
     $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="verif-applicants-index">
-    <div class = "custom_wrapper">
-        <div class="custom_header">
-            <a href="<?= Url::toRoute(['/subcomponents/admissions/admissions/index']);?>" title="Admissions Home">     
-                <img class="custom_logo_students" src ="css/dist/img/header_images/admissions.png" alt="admission-avatar">
-                <span class="custom_module_label">Welcome to the Admissions Management System</span> 
-                <img src ="css/dist/img/header_images/admissions.png" alt="admission-avatar" class="pull-right">
-            </a>    
-        </div>
-        
-        <div class="custom_body">
-            <h1 class="custom_h1"><?= Html::encode($this->title) ?></h1>
-            <!-- Dashboard buttons -->
-            <div class="box">
-                <?php if (Yii::$app->user->can('verifyApplicants')): ?>
-                    <div class="box-body">
-                        <a style="width:10%; margin-left:15%; margin-right:10%; margin-top:5%; margin-bottom:5%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-pending', 
-                            'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
-                            <span class="badge bg-green" style="font-size:16px;"><?= $pending ?></span>
-                            <i class="fa fa-cart-plus"></i> <strong>Pending</strong>
-                         </a>
+<div class="page-header text-center no-padding">
+    <a href="<?= Url::toRoute(['/subcomponents/admissions/verify-applicants']);?>" title="Process Applications">
+        <h1>Welcome to the Admissions Management System</h1>
+    </a>
+</div>
 
-                        <a style="width:10%; margin-right:10%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-verified',
-                            'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
-                            <span class="badge bg-green" style="font-size:16px;"><?= $verified ?></span>
-                            <i class="fa fa-check"></i> <strong>Verified</strong>
-                         </a>
+<section class="content-header">
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
+</section><br/>
 
-                        <a style="width:10%; margin-right:10%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-queried',
-                            'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
-                            <span class="badge bg-green" style="font-size:16px;"><?= $queried ?></span>
-                            <i class="fa fa-question"></i> <strong>Queries</strong>
-                         </a>
+<h2 class="text-center"><?= $this->title;?></h2>
 
-                        <a style="width:10%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-all',
-                            'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
-                            <span class="badge bg-green" style="font-size:16px;"><?= $total ?></span>
-                            <i class="fa fa-users"></i> <strong>All</strong>
-                         </a>
-                    </div>
-                <?php endif; ?>
+<div class="box box-primary table-responsive no-padding" style = "font-size:1.1em">
+    <div class="box-body">
+         <!-- Dashboard buttons -->
+        <?php if (Yii::$app->user->can('verifyApplicants')): ?>
+            <div class="box-body">
+                <a style="width:10%; margin-left:15%; margin-right:10%; margin-top:5%; margin-bottom:5%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-pending', 
+                    'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
+                    <span class="badge bg-green" style="font-size:16px;"><?= $pending ?></span>
+                    <i class="fa fa-cart-plus"></i> <strong>Pending</strong>
+                 </a>
+
+                <a style="width:10%; margin-right:10%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-verified',
+                    'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
+                    <span class="badge bg-green" style="font-size:16px;"><?= $verified ?></span>
+                    <i class="fa fa-check"></i> <strong>Verified</strong>
+                 </a>
+
+                <a style="width:10%; margin-right:10%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-queried',
+                    'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
+                    <span class="badge bg-green" style="font-size:16px;"><?= $queried ?></span>
+                    <i class="fa fa-question"></i> <strong>Queries</strong>
+                 </a>
+
+                <a style="width:10%; font-size:16px;" class="btn btn-app" href="<?= Url::to(['verify-applicants/view-all',
+                    'cseccentreid' => $centre_id, 'centrename' => $centre_name])?>">
+                    <span class="badge bg-green" style="font-size:16px;"><?= $total ?></span>
+                    <i class="fa fa-users"></i> <strong>All</strong>
+                 </a>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
 </div>
--- frontend/subcomponents/admissions/views/verify-applicants/index.php
+++ frontend/subcomponents/admissions/views/verify-applicants/index.php
@@ -1,40 +1,44 @@
 <?php
+    use yii\widgets\Breadcrumbs;
+    use yii\helpers\Html;
+    use yii\grid\GridView;
+    use yii\helpers\Url;
 
-use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\helpers\Url;
+    use frontend\models\Application;
+    use frontend\models\ApplicationPeriod;
+    use frontend\models\Division;
+    use frontend\models\EmployeeDepartment;
 
-use frontend\models\Application;
-use frontend\models\ApplicationPeriod;
-use frontend\models\Division;
-use frontend\models\EmployeeDepartment;
-
-
-/* @var $this yii\web\View */
-/* @var $searchModel frontend\models\CsecCentreSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
-$this->title = 'Verify Applicants';
-$this->params['breadcrumbs'][] = $this->title;
+    $this->title = 'Verify Applicants';
+    $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="verify-applicants-index">
-    <div class = "custom_wrapper">
-        <div class="custom_header">
-            <a href="<?= Url::toRoute(['/subcomponents/admissions/admissions/index']);?>" title="Admissions Home">     
-                <img class="custom_logo_students" src ="css/dist/img/header_images/admissions.png" alt="admission-avatar">
-                <span class="custom_module_label">Welcome to the Admissions Management System</span> 
-                <img src ="css/dist/img/header_images/admissions.png" alt="admission-avatar" class="pull-right">
-            </a>    
-        </div><br/>
-        
+<div class="page-header text-center no-padding">
+    <a href="<?= Url::toRoute(['/subcomponents/admissions/verify-applicants']);?>" title="Process Applications">
+        <h1>Welcome to the Admissions Management System</h1>
+    </a>
+</div>
+
+<section class="content-header">
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
+</section><br/>
+
+<h2 class="text-center"><?= $this->title;?></h2>
+
+
+<div class="box box-primary table-responsive no-padding" style = "font-size:1.1em">
+    <div class="box-header with-border">
+        <span class="box-title">Open Application Period Statistics</span>
+     </div>
+    
+    <div class="box-body">
         <?php if (EmployeeDepartment::getUserDivision() == 1):?>
-            <p id="offer-message" class="alert alert-info" role="alert" style="width: 95%; margin: 0 auto; font-size:16px; padding-top:15px; padding-bottom:30px;"> 
+            <p id="offer-message" class="alert alert-info" role="alert" style="width: 98%; margin: 0 auto; font-size:16px; padding-top:15px; padding-bottom:30px;"> 
                 <span class="pull-left"><strong >Total Applicants Received: <?= Application::countActiveApplications();?></strong></span>
                 <span class="pull-right"><strong>Total Applicants Verified: <?= Application::countVerifiedApplications();?></strong></span>
             </p><br/>
         <?php endif;?>
-        
+            
         <?php
             $periods = ApplicationPeriod::periodIncomplete();
             if ($periods == true)
@@ -43,7 +47,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 {
                     if (EmployeeDepartment::getUserDivision() == 1  || EmployeeDepartment::getUserDivision() == $period->divisionid)
                     {
-                        echo "<p class='alert alert-info' role='alert' style='width: 95%; margin: 0 auto; font-size:16px; padding-top:15px; padding-bottom:30px;'>"; 
+                        echo "<p class='alert alert-info' role='alert' style='width: 98%; margin: 0 auto; font-size:16px; padding-top:15px; padding-bottom:30px;'>"; 
                             echo "<span class='pull-left'><strong >";
                                 echo  Division::getDivisionAbbreviation($period->divisionid) . " Applications Received:" . Application::countActiveApplications($period->divisionid);
                             echo "</strong></span>";
@@ -55,73 +59,78 @@ $this->params['breadcrumbs'][] = $this->title;
                     }
                 }
             }
-            
        ?>
-        
-        <div class="custom_body">
-            <h1 class="custom_h1"><?= Html::encode($this->title) ?></h1>
-           
-            <?php if ($dataProvider == true):?>
-                <?= GridView::widget([
-                    'dataProvider' => $dataProvider,
-                    'options' => [
-                            'style' => 'width:95%; margin: 0 auto;'
-                        ],
-                    'columns' => [
-                        ['class' => 'yii\grid\SerialColumn'],
-                        [
-                            'attribute' => 'centre_name',
-                            'format' => 'html',
-                            'label' => 'Centre Name',
-                            'value' => function($row)
-                                {
-                                   return Html::a($row['centre_name'], 
-                                           Url::to(['verify-applicants/centre-details', 'centre_id' => $row['centre_id'], 'centre_name' => $row['centre_name']]));
-                                }
-                        ],
-                        [
-                            'attribute' => 'status',
-                            'format' => 'text',
-                            'label' => 'Status'
-                        ],
-                        [
-                            'attribute' => 'applicants_verified',
-                            'format' => 'text',
-                            'label' => 'Applicants Verified'
-                        ],
-                        [
-                            'attribute' => 'total_received',
-                            'format' => 'text',
-                            'label' => 'Total Received Applicants'
-                        ],
-                        [
-                            'format' => 'html',
-                            'label' => 'Percentage Completed',
-                            'value' => function($row)
-                                {
-                                        $value = $row['percentage_completed'];
-                                       return 
-                                        "<small class='pull-right'>$value%</small>
-                                         <div class='progress xs'>
-                                          <div class='progress-bar progress-bar-green' style='width: $value%' role='progressbar' aria-valuenow='$value' aria-valuemin='0' aria-valuemax='100'>
-                                            <span class='sr-only'>$value%</span>
-                                          </div>
-                                         </div>
-                                        ";
-                                }
-                        ],
+    </div>
+</div><br/>
+
+
+<div class="box box-primary table-responsive no-padding" style = "font-size:1.1em">
+    <div class="box-header with-border">
+        <span class="box-title">Examination Centre Listing</span>
+     </div>
+    
+    <div class="box-body">
+        <?php if ($dataProvider == true):?>
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'options' => [
+                        'style' => ''
                     ],
-                ]); ?>
-            
-            <?php else:?>
-                <br/><div class="alert alert-info" role="alert" style="width: 90%; margin: 0 auto; font-size:20px; text-align:center">
-                    <p>There are no current applications, pending verification.</p>
-                </div>
-            <?php endif;?>
-                
-                
-            <br/><br/><br/>    
-            <?php if(Application::getAllAbandonedApplicantApplications() == true):?>
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
+                    [
+                        'attribute' => 'centre_name',
+                        'format' => 'html',
+                        'label' => 'Centre Name',
+                        'value' => function($row)
+                            {
+                               return Html::a($row['centre_name'], 
+                                       Url::to(['verify-applicants/centre-details', 'centre_id' => $row['centre_id'], 'centre_name' => $row['centre_name']]));
+                            }
+                    ],
+                    [
+                        'attribute' => 'status',
+                        'format' => 'text',
+                        'label' => 'Status'
+                    ],
+                    [
+                        'attribute' => 'applicants_verified',
+                        'format' => 'text',
+                        'label' => 'Applicants Verified'
+                    ],
+                    [
+                        'attribute' => 'total_received',
+                        'format' => 'text',
+                        'label' => 'Total Received Applicants'
+                    ],
+                    [
+                        'format' => 'html',
+                        'label' => 'Percentage Completed',
+                        'value' => function($row)
+                            {
+                                    $value = $row['percentage_completed'];
+                                   return 
+                                    "<small class='pull-right'>$value%</small>
+                                     <div class='progress xs'>
+                                      <div class='progress-bar progress-bar-green' style='width: $value%' role='progressbar' aria-valuenow='$value' aria-valuemin='0' aria-valuemax='100'>
+                                        <span class='sr-only'>$value%</span>
+                                      </div>
+                                     </div>
+                                    ";
+                            }
+                    ],
+                ],
+            ]); ?>
+
+        <?php else:?>
+            <br/><div class="alert alert-info" role="alert" style="width: 90%; margin: 0 auto; font-size:20px; text-align:center">
+                <p>There are no current applications, pending verification.</p>
+            </div>
+        <?php endif;?>
+    </div>
+    
+    <div class="box-footer">
+        <?php if(Application::getAllAbandonedApplicantApplications() == true):?>
                 <fieldset style='width:95%; margin:0 auto;'>
                     <legend>Abandoned Applications</legend>
                     
@@ -134,8 +143,5 @@ $this->params['breadcrumbs'][] = $this->title;
                     <td><a class="btn btn-primary pull-right" href=<?=Url::toRoute(['/subcomponents/admissions/verify-applicants/index-abandoned']);?> role="button"> View Abandoned Applications</a></td>
                 </fieldset>
             <?php endif;?>
-            
-        </div>
     </div>
-
 </div>
