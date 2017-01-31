4c83868f352ced9a198aac25c0c951a7358267bd
Laurence Charles <charles.laurence1@gmail.com>
2017-01-31 19:07:57 -0400

    Upgraded UI for Holds

--- frontend/subcomponents/students/views/profile/add_hold.php
+++ frontend/subcomponents/students/views/profile/add_hold.php
@@ -1,11 +1,5 @@
 <?php
-
-/* 
- * 'add_hold' view.  Used for modifying information in the 'General' section of 'Profile' tab
- * Author: Laurence Charles
- * Date Created: 04/01/2016
- */
-
+    use yii\widgets\Breadcrumbs;
     use yii\helpers\Html;
     use yii\helpers\Url;
     use yii\widgets\ActiveForm;
@@ -21,49 +15,43 @@
         $hold_type = "Library";
     
     $this->title = 'Add Hold';
+    
+    $this->params['breadcrumbs'][] = ['label' => 'Find An Student', 'url' => Url::toRoute(['/subcomponents/students/student/find-a-student'])];
+    $this->params['breadcrumbs'][] = ['label' => 'Student Profile', 'url' => Url::toRoute(['profile/student-profile', 'personid' => $personid, 'studentregistrationid' => $studentregistrationid])];
+    $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-    <div class="site-index">
-        <div class = "custom_wrapper">
-            <div class="custom_header">
-                <a href="<?= Url::toRoute(['/subcomponents/students/student/find-a-student']);?>" title="Find A Student">     
-                    <img class="custom_logo_students" src ="css/dist/img/header_images/sms_4.png" alt="student avatar">
-                    <span class="custom_module_label">Welcome to the Student Management System</span> 
-                    <img src ="css/dist/img/header_images/sms_4.png" alt="student avatar" class="pull-right">
-                </a>    
-            </div>
-            
-            <div class="custom_body">  
-                <h1 class="custom_h1">Add New <?=$hold_type?> Hold</h1>
+<div class="page-header text-center no-padding">
+    <a href="<?= Url::toRoute(['/subcomponents/students/student/find-a-student']);?>" title="Find A Student">
+        <h1>Welcome to the Student Management System</h1>
+    </a>
+</div>
 
-                <?php
-                    $form = ActiveForm::begin([
-                                //'action' => Url::to(['gradebook/index']),
-                                'id' => 'add-hold',
-                                'options' => [
-//                                    'class' => 'form-layout'
-//                                    'class' => 'form-inline',
-                                ],
-                            ]);
+<section class="content-header">
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
+</section><br/><br/>
 
-                        echo "<table class='table table-hover' style='margin: 0 auto;'>";                                        
-                            echo "<tr>";
-                                echo "<th style='vertical-align:middle'>Hold Type</th>";
-                                echo "<td>{$form->field($hold, 'holdtypeid')->label('')->dropDownList(Hold::initializeHoldList($categoryid), ['style'=> 'font-size:14px;'])}</td>";
-                            echo "</tr>";
-
-                            echo "<tr>";
-                                echo "<th style='vertical-align:middle'>Details</th>";
-                                echo "<td>{$form->field($hold, 'details')->label('')->textInput(['maxlength' => true, 'style'=> 'font-size:14px;'])}</td>";
-                            echo "</tr>";
-                        echo "</table>";
-
-                        echo Html::a(' Cancel',['profile/student-profile', 'personid' => $personid, 'studentregistrationid' => $studentregistrationid], ['class' => 'btn btn-block btn-lg btn-danger glyphicon glyphicon-remove-circle pull-left', 'style' => 'width:25%; margin-left:15%;']);
-                        echo Html::submitButton(' Save', ['class' => 'glyphicon glyphicon-ok btn btn-block btn-lg btn-success pull-right', 'style' => 'width:25%; margin-right:15%;']);
-
-                    ActiveForm::end();    
-                ?>
-            </div>
-        </div>
+<div class="box box-primary table-responsive no-padding" style = "font-size:1.2em;">
+    <div class="box-header with-border">
+        <span class="box-title">Add New <?=$hold_type?> Hold</span>
     </div>
+    
+    <?php $form = ActiveForm::begin(); ?>
+        <div class="box-body">
+            <div class="form-group">
+               <label class="control-label col-xs-6 col-sm-5 col-md-5 col-lg-3" for="holdtypeid">Hold Type:</label>
+               <?= $form->field($hold, 'holdtypeid')->label('')->dropDownList(Hold::initializeHoldList($categoryid), ["class" => "no-padding col-xs-6 col-sm-7 col-md-7 col-lg-9"]) ?>
+           </div>
+            
+            <div class="form-group">
+               <label class="control-label col-xs-6 col-sm-5 col-md-5 col-lg-3" for="details'">Details:</label>
+               <?= $form->field($hold, 'details')->label('')->textInput(["class" => "no-padding col-xs-6 col-sm-7 col-md-7 col-lg-9"])?>
+           </div>
+        </div>
 
+        <div class="box-footer pull-right">
+            <?= Html::submitButton(' Submit', ['class' => 'btn btn-success', 'style' => 'margin-right:20px']);?>
+            <?= Html::a(' Cancel', ['profile/student-profile', 'personid' => $personid, 'studentregistrationid' => $studentregistrationid], ['class' => 'btn  btn-danger']);?>
+        </div>
+    <?php ActiveForm::end(); ?>   
+</div>
