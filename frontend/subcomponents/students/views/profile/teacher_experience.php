5485bf88607f40c089ed1c6d7219ae8dc6094ae2
Laurence Charles <charles.laurence1@gmail.com>
2017-02-01 11:59:56 -0400

    Patched student_profile view links v2

--- frontend/subcomponents/students/views/profile/teacher_experience.php
+++ frontend/subcomponents/students/views/profile/teacher_experience.php
@@ -16,7 +16,7 @@
 ?>
 
 
-div class="page-header text-center no-padding">
+<div class="page-header text-center no-padding">
     <a href="<?= Url::toRoute(['/subcomponents/students/student/find-a-student']);?>" title="Find A Student">
         <h1>Welcome to the Student Management System</h1>
     </a>
