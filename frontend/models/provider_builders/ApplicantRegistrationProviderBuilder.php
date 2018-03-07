acacdb047f314af0eb10ffe7dea1f4df317cb899
Laurence Charles <charles.laurence1@gmail.com>
2018-03-07 18:29:10 -0400

    Update applicant status algorithm v8

--- frontend/models/provider_builders/ApplicantRegistrationProviderBuilder.php
+++ frontend/models/provider_builders/ApplicantRegistrationProviderBuilder.php
@@ -95,7 +95,7 @@
                 'allModels' => $records,
                 'pagination' => ['pageSize' => $page_size],
                 'sort' => [
-                    'defaultOrder' => ['applicantregistrationid' =>  SORT_DESC, 'email' => SORT_ASC],
+                    'defaultOrder' => ['email' => SORT_ASC],
                     'attributes' => ['email', 'firstname', 'lastname']]
                 ]); 
             
@@ -186,7 +186,7 @@
                 'allModels' => $records,
                 'pagination' => ['pageSize' => $page_size],
                 'sort' => [
-                    'defaultOrder' => [ 'applicantregistrationid' =>  SORT_DESC, 'email' => SORT_ASC],
+                    'defaultOrder' => ['email' => SORT_ASC],
                     'attributes' => ['email', 'firstname', 'lastname']]
                 ]); 
             
@@ -277,7 +277,7 @@
                 'allModels' => $records,
                 'pagination' => ['pageSize' => $page_size],
                 'sort' => [
-                    'defaultOrder' => ['applicantname' =>  SORT_DESC, 'email' => SORT_ASC],
+                    'defaultOrder' => ['email' => SORT_ASC],
                     'attributes' => ['email', 'firstname', 'lastname']]
                 ]); 
             
