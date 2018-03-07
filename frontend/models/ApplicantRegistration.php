dff3f5471d457e808b1f1901b9fbb55f00edbc72
Laurence Charles <charles.laurence1@gmail.com>
2018-03-07 18:38:42 -0400

    Update applicant status algorithm v10

--- frontend/models/ApplicantRegistration.php
+++ frontend/models/ApplicantRegistration.php
@@ -196,7 +196,7 @@
                                 ->all();
                          if ($submitted_applications == true)   //if applicant has submitted their application
                          {
-                             if (Applicant::isAbandoned($personid) == true)
+                             if (Applicant::isAbandoned($account_pending_test1->personid) == true)
                             {
                                 $status = "Removed";
                             }
