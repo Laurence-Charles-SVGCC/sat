2f9846fb08e7adee5b8ee96e7287241bdc979fbd
SoftwareEngineer <softwareengineer@admins-iMac-4.local>
2018-07-17 08:52:19 -0400

    Modified welcome page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,10 +9,10 @@
 
     <h4>
         <div class = "alert alert-info">
-            Welcome to your control panel. Please contact the the E-College Unit for assistance, if the controls 
-            you require are absent .
+            Welcome to your control panel. Please contact the the E-College Unit for assistance, if the controls
+            you require are not present.
         </div>
     <h4>
-            
+
     <!--<img src="css/dist/img/college_home.png" alt="octagon" style="width:100%;">-->
 </div>
