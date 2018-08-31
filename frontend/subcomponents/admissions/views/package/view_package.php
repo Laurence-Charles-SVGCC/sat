e10608f55a87b8fbdd6208fe4fbece18066d3478
SoftwareEngineer <softwareengineer@admins-iMac-4.local>
2018-08-31 15:23:35 -0400

    Patched application period field

--- frontend/subcomponents/admissions/views/package/view_package.php
+++ frontend/subcomponents/admissions/views/package/view_package.php
@@ -6,13 +6,13 @@
     use yii\bootstrap\Modal;
     use yii\bootstrap\ActiveField;
     use yii\helpers\ArrayHelper;
-    
+
     use common\models\User;
     use frontend\models\ApplicationPeriod;
     use frontend\models\Package;
     use frontend\models\PackageType;
     use frontend\models\PackageProgress;
-    
+
     $document_count = [
         0 => 'No attachments',
         1 => '1',
@@ -26,7 +26,7 @@
         9 => '9',
         10 => '10',
     ];
-    
+
     $this->title = 'View Package';
     $this->params['breadcrumbs'][] = ['label' => 'Packages', 'url' => Url::toRoute(['/subcomponents/admissions/package'])];
     $this->params['breadcrumbs'][] = $this->title;
@@ -37,14 +37,14 @@
     <div class="box-header with-border">
         <span class="box-title"><?= $this->title?></span>
     </div>
-     
+
      <div class="box-body">
          <fieldset id="config-package">
              <legend><strong>Package Details</strong></legend>
             <?php
                 $form = ActiveForm::begin();
                     echo "<br/>";
-                    echo "<table class='table table-hover' style='width:100%; margin: 0 auto;'>";   
+                    echo "<table class='table table-hover' style='width:100%; margin: 0 auto;'>";
                         echo "<tr>";
                             echo "<th style='width:25%; vertical-align:middle'> Package Name</th>";
                             echo "<td>{$form->field($package, 'name')->label('', ['class'=> 'form-label'])->textInput(['maxlength' => true, 'readonly' => true])}</td>";
@@ -52,7 +52,7 @@
 
                         echo "<tr>";
                             echo "<th style='width:25%; vertical-align:middle'>Application Period</th>";
-                            echo "<td>{$form->field($package, 'applicationperiodid')->label('', ['class'=> 'form-label'])->dropDownList(ArrayHelper::map(ApplicationPeriod::periodIncomplete(), 'applicationperiodid', 'name'), ['readonly' => true, 'disabled' => true])}</td>";
+                            echo "<td>{$form->field($package, 'applicationperiodid')->label('', ['class'=> 'form-label'])->dropDownList(ArrayHelper::map(ApplicationPeriod::find()->all(), 'applicationperiodid', 'name'), ['readonly' => true, 'disabled' => true])}</td>";
                         echo "</tr>";
 
                         echo "<tr>";
@@ -71,12 +71,12 @@
                         echo "<tr>";
                             echo "<th style='width:25%; vertical-align:middle'>Number of Attachments</th>";
                             echo "<td>{$form->field($package, 'documentcount')->label('', ['class'=> 'form-label'])->dropDownList($document_count, ['readonly' => true, 'disabled' => true])}</td>";
-                        echo "</tr>"; 
+                        echo "</tr>";
 
                         echo "<tr>";
                             echo "<th style='width:25%; vertical-align:middle'>Email Title</th>";
                             echo "<td>{$form->field($package, 'emailtitle')->label('', ['class'=> 'form-label'])->textInput(['maxlength' => true, 'readonly' => true])}</td>";
-                        echo "</tr>"; 
+                        echo "</tr>";
 
                         echo "<tr>";
                             echo "<th style='width:25%; vertical-align:middle'>Email Introductory Statements</th>";
@@ -92,20 +92,20 @@
                                 $text= date("l F j, Y") . "                                                                                                 "
                                         . "                                                                                     Dear [firstname] [lastname],"
                                         . "                                                                                                                 "
-                                        . "                                                                                      "  
+                                        . "                                                                                      "
                                         . "We are pleased to inform you that you have been invited to interview for a place in the  "
                                         . "[programme name] at the [division_name] commencing on " . $package->commencementdate .  ".";
                                 echo "<td>";
                                     echo Html::textarea('email-intro', $text, ['rows' => 10, 'maxlength' => true, 'style' => 'font-size:14px; width:100%', 'disabled' => true]);
                                 echo "</td>";
-                            }        
+                            }
                             elseif ($package->packageid  && $package->packagetypeid==4)
                             {
-                                $text= date("l F j, Y") . "                                                                                                  "                                                                                                  
+                                $text= date("l F j, Y") . "                                                                                                  "
                                         . "                                                                                     Dear [firstname] [lastname],"
                                         . "                                                                                                                 "
-                                        . "                                                                                      "  
-                                        . "We are pleased to inform you that your application to the St. Vincent and the Grenadines Community College has been successful." 
+                                        . "                                                                                      "
+                                        . "We are pleased to inform you that your application to the St. Vincent and the Grenadines Community College has been successful."
                                         . "  You are offered a place in the [programme name] at the [division_name] commencing on " . $package->commencementdate .  ".      "
                                         . "Your Student Number is: [student number]";
                                 echo "<td>";
@@ -117,9 +117,9 @@
                         echo "<tr>";
                             echo "<th style='width:25%; vertical-align:middle'>Email Content</th>";
                             echo "<td>{$form->field($package, 'emailcontent')->label('', ['class'=> 'form-label'])->textArea(['rows' => 50, 'maxlength' => true, 'style' => 'font-size:14px;', 'readonly' => true])}</td>";
-                        echo "</tr>"; 
-                    echo "</table>"; 
-                ActiveForm::end();    
+                        echo "</tr>";
+                    echo "</table>";
+                ActiveForm::end();
             ?>
         </fieldset><br/><br/>
 
@@ -132,7 +132,7 @@
                 <ul>
                     <?php foreach($saved_documents as $index=>$doc):?>
                         <li><?=substr($doc,42)?></li>
-                    <?php endforeach;?>    
+                    <?php endforeach;?>
                 </ul>
             <?php endif;?>
         </fieldset><br/>
