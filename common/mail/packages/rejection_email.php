ba6027ff9a06dffb57e9e987c5a3f26d726a13c6
SoftwareEngineer <softwareengineer@admins-iMac-4.local>
2018-11-08 16:37:57 -0400

    feature: update rejection email template

--- common/mail/packages/rejection_email.php
+++ common/mail/packages/rejection_email.php
@@ -8,19 +8,19 @@
         <p>Dear <?= $first_name . ' ' . $last_name ?>,</p>
     </div>
 
-    <div id="body" style="white-space: pre-wrap;">
+    <div id="body">
         <?= $package->emailcontent?>
     </div>
 
     <div id="salutations">
         <p>With warm wishes and kind regards,</p>
-        
+
         <?php if (stripos(Url::home(true), "localhost") == false) :?>
            <p><img src="http://www.svgcc.vc/subdomains/sat/frontend/images/signature.png" alt="mrs-rouse-signature"></p>
         <?php else: ?>
              <p><img src="http://localhost/sat_dev/frontend/images/signature.jpg" alt="mrs-rouse-signature"></p>
         <?php endif; ?>
-             
+
         <p>
             Samantha Minors-Rouse
             <br/>Registrar
