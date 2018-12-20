b1729569c87eb624f77289dca2ea88f81efd8357
Laurence Charles <charles.laurence1@gmail.com>
2018-12-20 09:21:56 -0400

    fix: remove banner and reformat template [15]

--- common/mail/packages/dne_full_offer_email.php
+++ common/mail/packages/dne_full_offer_email.php
@@ -1,37 +1,47 @@
-<?php
-    use yii\helpers\Html;
-    use yii\web\UrlManager;
-    use yii\helpers\Url;
-    use frontend\models\ApplicationPeriod;
-?>
+<div>
+  <div>
+    <p><?= date("l F j, Y"); ?></p>
+    <p>Dear <?= $first_name . ' ' . $last_name ?>,</p>
 
-    <html>
-        <div class="conditional_offer_email">
-            <img src="http://www.svgcc.vc/subdomains/sat/frontend/images/header.png" alt="header" class="img-rounded" style="width:100%; height:150px;">
-            
-            <div id="introduction">
-                <p><?= date("l F j, Y"); ?></p>
-                <p>Dear <?= $first_name . ' ' . $last_name ?>,</p>
-                
-                <p>
-                    We are pleased to inform you that your application to the St. Vincent and the Grenadines Community College has been successful.  
-                    You are offered a place in the <?= $programme; ?> Programme at the <?= $division_name ?> commencing on <?= $package->commencementdate?>.<br/>
-                    Your Student Number is: <?= $studentno; ?>.
-                </p>
-            </div>
+    <p>
+      We are pleased to inform you that your application to the St. Vincent and
+      the Grenadines Community College has been successful. You are offered a
+      place in the <?= $programme; ?> Programme at the <?= $division_name ?>
+      commencing on <?= $package->commencementdate?>.<br/>
+    </p>
 
-            <div id="body" style="white-space: pre;">
-                <?= $package->emailcontent?>
-            </div>
+    <p>
+      <strong>
+        Your Student Number is: <?= $applicant->potentialstudentid; ?>.
+      </strong>
+    </p>
+  </div>
 
-            <div id="salutations">
-                <p>With warm wishes and kind regards,</p>
-                <p><img src="http://www.svgcc.vc/subdomains/sat/frontend/images/signature.png" alt="mrs-rouse-signature"></p>
-                <!--<p><img src="http://localhost/sat_dev/frontend/images/signature.jpg" alt="mrs-rouse-signature"></p>-->
-                <p>
-                    Samantha Minors-Rouse
-                    <br/>Registrar
-                </p>
-            </div>
-        </div>
-    </html>
+  <div style="white-space: pre;">
+    <?= $package->emailcontent?>
+  </div>
+
+  <div>
+    <p>With warm wishes and kind regards,</p>
+    <?php if (stripos(yii\helpers\Url::home(true), "localhost") == false) :?>
+      <p>
+        <img src="http://www.svgcc.vc/subdomains/sat/frontend/images/signature.png"
+        alt="mrs-rouse-signature">
+      </p>
+    <?php else: ?>
+      <p>
+        <img src="http://localhost/sat_dev/frontend/images/signature.jpg"
+        alt="mrs-rouse-signature">
+      </p>
+    <?php endif; ?>
+
+    <p>
+      Samantha Minors-Rouse<br/>
+      Registrar
+    </p><br/>
+
+    <div style="white-space: pre;">
+      <?= $package->disclaimer?>
+    </div>
+  </div>
+</div>
