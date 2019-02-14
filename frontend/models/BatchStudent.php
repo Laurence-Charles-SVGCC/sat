5ab78cc849d03128738671296da076f49c3729c6
Laurence Charles <charles.laurence1@gmail.com>
2019-02-14 11:09:41 -0400

    feature: add condition to process the new batchtype labelled Absent

--- frontend/models/BatchStudent.php
+++ frontend/models/BatchStudent.php
@@ -87,14 +87,14 @@ class BatchStudent extends \yii\db\ActiveRecord
     {
         return $this->hasOne(CourseStatus::className(), ['coursestatusid' => 'coursestatusid']);
     }
-    
-    
+
+
     /**
      * Returns a count of the number of distinct semesters a student has grades for.
-     * 
+     *
      * @param type $studentregistrationid
      * @return type
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 10/12/2015
      * Dte Last Modified: 10/12/2015
@@ -110,19 +110,19 @@ class BatchStudent extends \yii\db\ActiveRecord
                 . " JOIN course_offering"
                 . " ON batch.courseofferingid = course_offering.courseofferingid"
                 . " WHERE batch_students.studentregistrationid = " .  $studentregistrationid
-                . ";"    
+                . ";"
                 )
                 ->queryScalar();
         return $count;
     }
-    
-    
+
+
     /**
      * Returns and array of associative arrays where each associative array holds a ['semesterid' => $semesterid, 'emester_title'=> $semester.title, 'academic_year_title' =>  $academic_year.title]
-     * 
+     *
      * @param type $studentregistrationid
      * @return type
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 11/12/2015
      * Dte Last Modified: 11/12/2015
@@ -138,7 +138,7 @@ class BatchStudent extends \yii\db\ActiveRecord
                 . " JOIN course_offering"
                 . " ON batch.courseofferingid = course_offering.courseofferingid"
                 . " WHERE batch_students.studentregistrationid = " .  $studentregistrationid
-                . ";"    
+                . ";"
                 )
                 ->queryAll();
         if (count ($semesters > 0))
@@ -149,14 +149,14 @@ class BatchStudent extends \yii\db\ActiveRecord
                 $keys = array();
                 $values = array();
                 $combined = array();
-                
+
                 $year = $db->createCommand(
                     "SELECT semester.semesterid AS 'sem_id', semester.title AS 'sem_title', academic_year.title AS 'year_title'"
                     . " FROM semester"
                     . " JOIN academic_year"
                     . " ON semester.academicyearid = academic_year.academicyearid"
                     . " WHERE semester.semesterid = " .  $semesters[$i]["semesterid"]
-                    . ";"    
+                    . ";"
                     )
                     ->queryOne();
                 if ($year["sem_id"])
@@ -168,8 +168,8 @@ class BatchStudent extends \yii\db\ActiveRecord
                     array_push($values, $year["sem_title"]);
                     array_push($values, $year["year_title"]);
                     $combined = array_combine($keys, $values);
-                    array_push($semester_info,$combined);    
-                    
+                    array_push($semester_info,$combined);
+
                     $keys = NULL;
                     $values = NULL;
                     $combined = NULL;
@@ -183,15 +183,15 @@ class BatchStudent extends \yii\db\ActiveRecord
         }
         return false;
     }
-    
-    
+
+
     /**
-     * Determines if $sem1 < $sem2 
-     * 
+     * Determines if $sem1 < $sem2
+     *
      * @param type $sem1
      * @param type $sem2
      * @return boolean
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 11/12/2015
      * Date Last Modified: 11/12/2015
@@ -211,13 +211,13 @@ class BatchStudent extends \yii\db\ActiveRecord
         }
         return false;
     }
-    
-    
+
+
     /**
      * Returns a sorted list of semesters
-     * 
+     *
      * @param type $semester_info
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 11/12/2015
      * Date Last Modified: 11/12/2015
@@ -240,15 +240,15 @@ class BatchStudent extends \yii\db\ActiveRecord
         }
         return $semester_info;
     }
-    
-     
+
+
     /**
      * Returns the number of courses taken in a particular semester by a particular student
-     * 
+     *
      * @param type $studentregistrationid
      * @param type $semesterid
      * @return type
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 10/12/2015
      * Date Last Modified: 29/01/2016
@@ -266,20 +266,20 @@ class BatchStudent extends \yii\db\ActiveRecord
                 . " ON batch.courseofferingid = course_offering.courseofferingid"
                 . " WHERE course_offering.semesterid = " .  $semesterid
                 . " AND batch_students.studentregistrationid = " . $studentregistrationid
-                . " AND batch_students.isactive = 1;"        
+                . " AND batch_students.isactive = 1;"
                 )
                 ->queryScalar();
         return $count;
     }
-    
-    
+
+
     /**
      * Returns the number of courses that should be considered for GPA calculation
-     * 
+     *
      * @param type $studentregistrationid
      * @param type $semesterid
      * @return type
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 13/01/2016
      * Date Last Modified: 29/01/2016
@@ -299,20 +299,20 @@ class BatchStudent extends \yii\db\ActiveRecord
                 . " AND batch_students.studentregistrationid = " . $studentregistrationid
                 . " AND batch_students.examtotal IS NOT NULL"
                 . " AND batch_students.courseworktotal IS NOT NULL"
-                . " AND batch_students.isactive = 1;"       
+                . " AND batch_students.isactive = 1;"
                 )
                 ->queryScalar();
         return $count;
     }
-    
-    
+
+
     /**
      * Returns and array of associative arrays where each associative array hold one modified 'batch_student' record
-     * 
+     *
      * @param type $studentregistrationid
      * @param type $semesterid
      * @return array
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 11/12/2015
      * Date Last Modified: 29/01/2016
@@ -355,7 +355,7 @@ class BatchStudent extends \yii\db\ActiveRecord
                 . " AND batch_students.isactive = 1;"
                 )
                 ->queryAll();
-        
+
         $records_container = array();
         for ($i = 0 ; $i < count($batch_student_records) ; $i++)
         {
@@ -378,7 +378,7 @@ class BatchStudent extends \yii\db\ActiveRecord
             array_push($keys, "course_status");
             array_push($keys, "grade");
             array_push($keys, "qualitypoints");
-            
+
             $values = array();
             array_push($values, $batch_student_records[$i]["batchid"]);
             array_push($values, $batch_student_records[$i]["batchtypeid"]);
@@ -389,7 +389,7 @@ class BatchStudent extends \yii\db\ActiveRecord
             array_push($values, $batch_student_records[$i]["examweight"]);
             array_push($values, $batch_student_records[$i]["passfailtypeid"]);
             array_push($values, $batch_student_records[$i]["code"]);
-            
+
             if ($batch_student_records[$i]["batchtypeid"] == 1)
             {
                 $batch_name = $batch_student_records[$i]["name"];
@@ -406,8 +406,12 @@ class BatchStudent extends \yii\db\ActiveRecord
             {
                 $batch_name = $batch_student_records[$i]["name"] . " - EXEMPTION";
             }
+            elseif ($batch_student_records[$i]["batchtypeid"]  == 5)
+            {
+                $batch_name = $batch_student_records[$i]["name"] . " - ABSENT";
+            }
             array_push($values, $batch_name);
-            
+
             array_push($values, $batch_student_records[$i]["credits_attempted"]);
             array_push($values, $batch_student_records[$i]["credits_awarded"]);
             array_push($values, $batch_student_records[$i]["courseworktotal"]);
@@ -416,7 +420,7 @@ class BatchStudent extends \yii\db\ActiveRecord
             array_push($values, $batch_student_records[$i]["course_status"]);
             array_push($values, $batch_student_records[$i]["grade"]);
             array_push($values, $batch_student_records[$i]["qualitypoints"]);
-            
+
             $combined = array_combine($keys, $values);
             array_push($records_container, $combined);
             $keys = NULL;
@@ -425,15 +429,15 @@ class BatchStudent extends \yii\db\ActiveRecord
         }
         return $records_container;
     }
-    
-    
+
+
     /**
      * Returns the GPA of a student for a particular semester
-     * 
+     *
      * @param type $studentregistrationid
      * @param type $semesterid
      * @return type
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 28/01/2016
      * Date Last Modified: 28/01/2016
@@ -444,9 +448,9 @@ class BatchStudent extends \yii\db\ActiveRecord
         $gradepoints_sum = 0;
         $credits_sum = 0;
         $semester_gpa = "Pending";
-        
+
         $count = count($semester_results);
-        
+
         for ($i=0 ; $i<$count ; $i++)
         {
             $grade_points = $semester_results[$i]['credits_attempted'] * $semester_results[$i]['qualitypoints'];
@@ -455,24 +459,24 @@ class BatchStudent extends \yii\db\ActiveRecord
                 && ($semester_results[$i]['passfailtypeid'] == 1 || $semester_results[$i]['passfailtypeid'] == 3))
             {
                 $gradepoints_sum += $grade_points;
-                $credits_sum += $semester_results[$i]["credits_awarded"]; 
+                $credits_sum += $semester_results[$i]["credits_awarded"];
             }
         }
-        
+
         if($gradepoints_sum!=0  && $credits_sum!=0)
             $semester_gpa = round(($gradepoints_sum/$credits_sum), 2);
-        
+
         return $semester_gpa;
     }
-    
-    
+
+
     /**
      * Returns a summary of a course information
-     * 
+     *
      * @param type $studentregistrationid
      * @param type $batchid
      * @return boolean
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 16/12/2015
      * Date Last Modified: 16/12/2015
@@ -486,7 +490,7 @@ class BatchStudent extends \yii\db\ActiveRecord
                 . " course_offering.coursetypeid AS 'coursetypeid',"
                 . " course_offering.passcriteriaid AS 'passcriteriaid',"
                 . " course_offering.passfailtypeid AS 'passfailtypeid',"
-                . " course_offering.passmark AS 'passmark',"              
+                . " course_offering.passmark AS 'passmark',"
                 . " course_offering.courseworkweight AS 'courseworkweight',"
                 . " course_offering.examweight AS 'examweight', "
                 . " course_catalog.coursecode AS 'code',"
@@ -510,20 +514,20 @@ class BatchStudent extends \yii\db\ActiveRecord
                 . " ON course_offering.coursecatalogid = course_catalog.coursecatalogid"
                 . " WHERE batch_students.batchid = " .  $batchid
                 . " AND batch_students.studentregistrationid = " . $studentregistrationid
-                . " AND batch_students.isactive = 1;"      
+                . " AND batch_students.isactive = 1;"
                 )
                 ->queryOne();
         if ($course_record)
             return $course_record;
         return false;
     }
-    
-    
+
+
     /**
      * Return true if student has failed course
-     * 
+     *
      * @return boolean
-     * 
+     *
      * Author: Laurence Charles
      * Date Created: 03/08/2017
      * Date Last Modified: 03/08/2017
@@ -536,7 +540,7 @@ class BatchStudent extends \yii\db\ActiveRecord
         }
         return false;
     }
-    
-    
-    
+
+
+
 }
