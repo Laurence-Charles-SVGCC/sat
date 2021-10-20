1e4209412ab561e1dd0f939520a6e446569af7d2
Laurence Charles <charles.laurence1@gmail.com>
2021-10-20 11:34:27 -0400

    Modify email template

--- common/mail/layouts/html.php
+++ common/mail/layouts/html.php
@@ -1,34 +1,33 @@
 <?php
-    use yii\helpers\Html;
+
+use yii\helpers\Html;
 use yii\helpers\Url;
 
 ?>
 
 <?php $this->beginPage() ?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
-            <title><?= Html::encode($this->title) ?></title>
-            <?php $this->head() ?>
-        </head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-        <body>
-            <?php $this->beginBody() ?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
 
-                <?php if (stripos(Url::home(true), "localhost") == false) :?>
-                  <img src="https://sat.svgcc.vc/images/email_header.png"
-                  alt="email_header" class="img-rounded"
-                  style="max-width:700px; max-height:150px"/>
-                <?php else: ?>
-                  <img src="http://localhost/sat_dev/frontend/web/img/email_header.png"
-                  alt="email_header" class="img-rounded"
-                  style="max-width:700px; max-height:150px"/>
-                <?php endif; ?>
+<body>
+    <?php $this->beginBody() ?>
 
-                <?= $content ?>
+    <?php if (stripos(Url::home(true), "localhost") == false) : ?>
+        <img src="https://sat.svgcc.vc/images/email_header.png" alt="email_header" class="img-rounded" style="max-width:700px; max-height:150px" />
+    <?php else : ?>
+        <img src="http://localhost:8888/sat_dev/frontend/web/img/email_header.png" alt="email_header" class="img-rounded" style="max-width:700px; max-height:150px" />
+    <?php endif; ?>
 
-            <?php $this->endBody() ?>
-        </body>
-    </html>
+    <?= $content ?>
+
+    <?php $this->endBody() ?>
+</body>
+
+</html>
 <?php $this->endPage() ?>
