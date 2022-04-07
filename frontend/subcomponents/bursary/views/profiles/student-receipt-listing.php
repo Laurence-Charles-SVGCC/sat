feeb45e763482ab2b1af9d849287428d261d809f
Laurence Charles (SVGCC) <laurence.charles@svgcc.vc>
2022-04-07 11:11:00 -0400

    Update student-receipt-listing.php
    Apply formatting to file

--- frontend/subcomponents/bursary/views/profiles/student-receipt-listing.php
+++ frontend/subcomponents/bursary/views/profiles/student-receipt-listing.php
@@ -7,56 +7,56 @@ use yii\helpers\Url;
 
 <div id="student-receipt-listing">
     <?=
-        GridView::widget(
-            [
-                "dataProvider" => $dataProvider,
-                "columns" => [
-                    [
-                        "label" => "Receipt Number",
-                        "format" => "raw",
-                        "value" => function ($row) {
-                            return Html::a(
-                                $row["receiptNumber"],
-                                Url::toRoute([
-                                    "payments/view-receipt",
-                                    "id" => $row["id"],
-                                    "username" => $row["username"],
-                                    "from" => "student-profile"
-                                ]),
-                                [
-                                    "title" => "View",
-                                    "style" => "margin:0px  20px"
-                                ]
-                            );
-                        }
-                    ],
-                    [
-                        "attribute" => "billingDetails",
-                        "format" => "text",
-                        "label" => "Billings"
-                    ],
-                    [
-                        "attribute" => "total",
-                        "format" => "text",
-                        "label" => "Total"
-                    ],
-                    [
-                        "format" => "raw",
-                        "label" => "Date Paid",
-                        "value" => function ($row) {
-                            return date_format(
-                                new \DateTime($row["datePaid"]),
-                                "F j, Y"
-                            );
-                        }
-                    ],
-                    [
-                        "attribute" => "applicationPeriod",
-                        "format" => "text",
-                        "label" => "Registration Period"
-                    ],
+    GridView::widget(
+        [
+            "dataProvider" => $dataProvider,
+            "columns" => [
+                [
+                    "label" => "Receipt Number",
+                    "format" => "raw",
+                    "value" => function ($row) {
+                        return Html::a(
+                            $row["receiptNumber"],
+                            Url::toRoute([
+                                "payments/view-receipt",
+                                "id" => $row["id"],
+                                "username" => $row["username"],
+                                "from" => "student-profile"
+                            ]),
+                            [
+                                "title" => "View",
+                                "style" => "margin:0px  20px"
+                            ]
+                        );
+                    }
                 ],
-            ]
-        );
+                [
+                    "attribute" => "billingDetails",
+                    "format" => "text",
+                    "label" => "Billings"
+                ],
+                [
+                    "attribute" => "total",
+                    "format" => "text",
+                    "label" => "Total"
+                ],
+                [
+                    "format" => "raw",
+                    "label" => "Date Paid",
+                    "value" => function ($row) {
+                        return date_format(
+                            new \DateTime($row["datePaid"]),
+                            "F j, Y"
+                        );
+                    }
+                ],
+                [
+                    "attribute" => "applicationPeriod",
+                    "format" => "text",
+                    "label" => "Registration Period"
+                ],
+            ],
+        ]
+    );
     ?>
 </div>
